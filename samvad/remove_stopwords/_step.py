d27ae5bd6cdfb4ff1f5b622b98947f645e3c8a23
amandesai01 <amandesai01@gmail.com>
2023-03-21 02:34:38 +0530

    remove_stopwords: Generalised with presets

--- samvad/remove_stopwords/_step.py
+++ samvad/remove_stopwords/_step.py
@@ -1,23 +1,11 @@
-from dataclasses import dataclass
-
-from samvad.core import api
+from samvad.core import api, presets
 
 from ._base import remove_stopwords
 
 
-@dataclass
-class RemoveStopwordsContext(api.Context):
-    input_text: str
-
-
-@dataclass
-class RemoveStopwordsOutput(api.Output):
-    filtered_text: str
-
-
 class RemoveStopwordsStep(api.Step):
     def run(self) -> None:
-        context: RemoveStopwordsContext = self.get_context(RemoveStopwordsContext)
+        context: presets.StringGeneralContext = self.get_context(presets.StringGeneralContext)
         filtered_text: str = remove_stopwords(context.input_text)
-        output: RemoveStopwordsOutput = RemoveStopwordsOutput(filtered_text)
+        output: presets.StringGeneralOutput = presets.StringGeneralOutput(filtered_text)
         self.set_output(output)
