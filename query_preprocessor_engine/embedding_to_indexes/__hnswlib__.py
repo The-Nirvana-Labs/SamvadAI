70c10518f6230065cb9b9facdd24cbbda4f8d63b
Dev Shailesh Shah <49101333+devshahofficial@users.noreply.github.com>
2023-03-21 14:59:14 -0400

    hnswlib: refactor

--- query_preprocessor_engine/embedding_to_indexes/__hnswlib__.py
+++ query_preprocessor_engine/embedding_to_indexes/__hnswlib__.py
@@ -1,25 +1,31 @@
-from hnswlib import Index
-from typing import List
+import hnswlib
+import numpy as np
 
-
-def embedding_to_indexes(embeddings: List[List[float]]) -> Index:
+def embedding_to_indexes(embeddings: np.ndarray) -> np.ndarray:
     """
     Builds a HNSWLIB index for the input embeddings using the given hyperparameters.
 
     Args:
-    - embeddings (List[List[float]]): The input embeddings to build the HNSWLIB index for.
+    - embeddings (np.ndarray): The input embeddings to build the HNSWLIB index for.
+                               Shape: (num_embeddings, embedding_dim)
 
     Returns:
-    - An instance of `Index` representing the HNSWLIB index.
+    - An np.ndarray representing the HNSWLIB index.
+      Shape: (num_embeddings, )
     """
     # Set hyperparameters
-    num_elements = len(embeddings)
-    dim = len(embeddings[0])
+    num_elements = embeddings.shape[0]
+    dim = embeddings.shape[1]
     max_elements = num_elements * 2
 
     # Build the index
-    index = Index(dim, "cosine")
+    index = hnswlib.Index(space='cosine', dim=dim)
     index.init_index(max_elements, ef_construction=100, M=64)
     index.add_items(embeddings)
 
-    return index
+    # Generate the index for all embeddings
+    index_array = np.zeros((num_elements,), dtype=int)
+    for i, embedding in enumerate(embeddings):
+        index_array[i] = index.knn_query(embedding, k=1)[0][0]
+
+    return index_array
