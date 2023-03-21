e1192d1314aea1e0f09cee65f76ca5954d77d8d1
Dev Shailesh Shah <49101333+devshahofficial@users.noreply.github.com>
2023-03-21 14:36:16 -0400

    annoy: refactor

--- query_preprocessor_engine/embedding_to_indexes/__annoy__.py
+++ query_preprocessor_engine/embedding_to_indexes/__annoy__.py
@@ -1,26 +1,33 @@
+import numpy as np
 from annoy import AnnoyIndex
-from typing import List
 
 
-def embedding_to_indexes(embeddings: List[List[float]]) -> AnnoyIndex:
+def embedding_to_indexes(embeddings: np.ndarray) -> np.ndarray:
     """
     Builds an Annoy index for the input embeddings using the given hyperparameters.
 
     Args:
-    - embeddings (List[List[float]]): The input embeddings to build the Annoy index for.
+    - embeddings (np.ndarray): The input embeddings to build the Annoy index for.
+                               Shape: (num_embeddings, embedding_dim)
 
     Returns:
-    - An instance of `AnnoyIndex` representing the Annoy index.
+    - An np.ndarray representing the Annoy index.
+      Shape: (num_embeddings, )
     """
     # Set hyperparameters
     n_trees = 100
     metric = 'angular'
-    embedding_dim = len(embeddings[0])
+    embedding_dim = embeddings.shape[1]
 
     # Build the index
     index = AnnoyIndex(embedding_dim, metric=metric)
     for i, embedding in enumerate(embeddings):
-        index.add_item(i, embedding)
+        index.add_item(i, embedding.tolist())
     index.build(n_trees)
 
-    return index
+    # Generate the index for all embeddings
+    index_array = np.zeros((embeddings.shape[0],), dtype=int)
+    for i, embedding in enumerate(embeddings):
+        index_array[i] = index.get_nns_by_vector(embedding.tolist(), 1)[0]
+
+    return index_array
