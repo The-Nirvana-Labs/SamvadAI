96d5567052a16f8fa0c060e84ce304505aa668f0
Dev Shailesh Shah <49101333+devshahofficial@users.noreply.github.com>
2023-03-25 18:03:19 -0400

    casting : tensor -> ndarray

---
+++ query_preprocessor_engine/tensor_to_ndarray.py
@@ -0,0 +1,17 @@
+import numpy as np
+import torch
+
+def cast_tensor_to_ndarray(x: torch.Tensor, use_gpu: bool = False) -> np.ndarray:
+    """
+    Casts a PyTorch tensor to a numpy ndarray.
+
+    Args:
+    - x (torch.Tensor): The input tensor to cast.
+    - use_gpu (bool): A flag indicating whether to use GPU or not.
+
+    Returns:
+    - A numpy ndarray with the same shape and data as the input tensor.
+    """
+    if use_gpu:
+        x = x.detach().cpu()
+    return x.detach().numpy()
