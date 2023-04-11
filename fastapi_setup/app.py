1e6d7370011929a3bcacfced3368d6fd781824ee
Dev Shailesh Shah <49101333+devshahofficial@users.noreply.github.com>
2023-04-10 23:20:50 -0400

    Errors fixed

--- fastapi_setup/app.py
+++ fastapi_setup/app.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+
+class TextPayload(BaseModel):
+    text: str
 
 
 def function_a(text: str) -> str:
@@ -13,15 +18,9 @@ def function_b(text: str) -> str:
     return text.replace(" ", "_")
 
 
-def function_c(text: str) -> str:
-    """Example function to modify the input string."""
-    return text[::-1]
-
-
 @app.post("/preprocessor/")
-def modify_string(text: str) -> str:
+def preprocessor(payload: TextPayload) -> str:
     """API endpoint to modify the input string using function_a, function_b, and function_c."""
-    text = function_a(text)
+    text = function_a(payload.text)
     text = function_b(text)
-    text = function_c(text)
     return text
