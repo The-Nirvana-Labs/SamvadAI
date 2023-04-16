f712182982807a1a6ddf34f8322e6682d9a523a2
Dev Shailesh Shah <49101333+devshahofficial@users.noreply.github.com>
2023-04-15 20:29:30 -0400

    data synthesis : entailer -> docs

---
+++ data_synthesis engine/entailor/__init__.py
@@ -0,0 +1,6 @@
+"""
+    Textual entailment is a technique that involves determining whether one piece of text logically implies another.
+    This can be useful for generating new data that is consistent with existing data, or for verifying the accuracy of
+    generated data. Textual entailment can be done using various natural language processing techniques, such as semantic
+    role labeling, dependency parsing, and machine learning algorithms.
+"""
