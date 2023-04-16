72ba32c2f57f40ad14e2a5143f5885e0179f7084
Dev Shailesh Shah <49101333+devshahofficial@users.noreply.github.com>
2023-04-15 20:25:08 -0400

    data synthesis : summarizer -> docs

---
+++ data_synthesis engine/summarizer/__init__.py
@@ -0,0 +1,6 @@
+"""
+    Text summarization is a technique that involves generating a concise and meaningful summary of a longer piece of
+    text. This can be done using various algorithms, such as extraction-based or abstraction-based methods, and can be
+    used to condense a large amount of information into a more manageable form. The goal of text summarization is to
+    preserve the most important information while discarding irrelevant details.
+"""
