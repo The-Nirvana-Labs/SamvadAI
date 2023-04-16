189d702c42cc190b49251c80207131981f5af59f
Dev Shailesh Shah <49101333+devshahofficial@users.noreply.github.com>
2023-04-15 20:30:23 -0400

    data synthesis : merginator -> docs

---
+++ data_synthesis engine/merginator/__init__.py
@@ -0,0 +1,6 @@
+"""
+    Sentence fusion involves combining two or more sentences into a single sentence while preserving the original
+    meaning. This technique can be used to create more complex sentences from simpler ones, or to generate new sentences
+    that contain information from multiple source sentences. Sentence fusion can be done manually or using automated
+    tools such as natural language processing algorithms.
+"""
